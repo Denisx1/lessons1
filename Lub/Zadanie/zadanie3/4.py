837f363d03805eb7f2c72425df4da25874fa6258
Denisx1 <tr20011974@mail.ru>
2021-11-11 17:12:52 +0200

    push

--- Lub/Zadanie/zadanie3/4.py
+++ Lub/Zadanie/zadanie3/4.py
@@ -1,11 +1,16 @@
 import random
-number = int(input('Введите число от 0 до 100 \n'))
+number = int(input('Введите число от 0 до 100\n'))
 a = random.randint(0,100)
 i = 1
 while i <= 9:
     if number != a:
-        number = int(input('Еще одна попытка\n'))
+        if number > a:
+            print(number, 'это число больше рандомного')
+        elif number < a:
+            print(number, 'это число меньше рандомного')    
+        number = int(input('Еще одна попытка '))
         i += 1
     else:
-        print('Krasava')    
+        print('Krasava')  
+        break  
 print('Вы не угадали ни разу но число случайное число = ' + str(a))  
