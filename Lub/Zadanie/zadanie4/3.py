24c6c06f03b74c1b11b98bfea3d0ee82d7c78104
Denisx1 <tr20011974@mail.ru>
2021-11-15 11:24:34 +0200

    func

---
+++ Lub/Zadanie/zadanie4/3.py
@@ -0,0 +1,11 @@
+j = int(input('Сколько чисел нужно поместить в список(Введите число )'))
+y = 1
+x = []
+while y <= j:
+    n = int(input())
+    x.append(n)
+    y += 1
+def to_list(*nums):
+    return sorted(x)
+print(to_list(n))        
+             
---
+++ Lub/Zadanie/zadanie4/4.py
@@ -0,0 +1,6 @@
+number = [1, 3, 6, 10, 12, 23, 10, 29, 12, 30]
+def useless(s):
+    number.sort
+    return s[-1] / len(s)
+print(number)       
+print(useless(number))
